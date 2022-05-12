faa0da5f3ea6580e88421edcbe8f362b44853b12
kartik-qapita <97021175+kartik-qapita@users.noreply.github.com>
2022-05-12 17:18:57 +0530

    Update Machine_setup.sh

--- Machine_setup.sh
+++ Machine_setup.sh
@@ -233,6 +233,18 @@ sudo docker container create -p 5341:5341 -p 8081:80 \
 sudo docker container start q-seq-node
 sudo docker container update --restart always q-seq-node
 
+#Installing VS Code
+
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 #Cloning Server & client
 # Clone the server repository and restore nuget packages
 
