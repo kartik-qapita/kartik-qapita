ec04429fe49ead38022a064b8b4b1bf62b808a52
kartik-qapita <kartik.g@qapitacorp.com>
2022-08-05 20:51:29 +0530

    added wall.sh

---
+++ Wall.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+#! /usr/bin/env zsh
+
+echo "Wallpaper Setup"
+
+sudo rm -rf /usr/share/backgrounds/Qapita.png
+
+sudo wget https://raw.githubusercontent.com/kartik-qapita/kartik-qapita/main/Qapita.png -P /usr/share/backgrounds
+
+gsettings set org.gnome.desktop.background picture-uri "file:////usr/share/backgrounds/Qapita.png"
