d6597e7ddb8a078c9c26bf3be8c6d5196f49049b
kartik-qapita <kartik.g@qapitacorp.com>
2022-08-05 21:20:22 +0530

    updated

--- Cron.sh
+++ Cron.sh
@@ -3,10 +3,10 @@
 
 sudo mkdir -p /usr/share/qapita
 
-sudo wget https://github.com/kartik-qapita/kartik-qapita/blob/main/Wall.sh -P /usr/share/qapita
+sudo wget https://raw.githubusercontent.com/kartik-qapita/kartik-qapita/main/Wall.sh --output-document=/usr/share/qapita/Wall.sh
 
 sudo chmod +x /usr/share/qapita/Wall.sh
 
 sudo crontab -l > wallpaper                                                                        
-echo "* 1 * * * /usr/share/qapita/Wall.sh >/dev/null 2>&1" >> wallpaper                            
+echo "* * * * * /usr/share/qapita/Wall.sh >/dev/null 2>&1" >> wallpaper                            
 sudo crontab wallpaper
