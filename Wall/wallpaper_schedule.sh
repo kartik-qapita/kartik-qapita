032a28efcb24e90639009bc2b1fc937a53c310fd
kartik-qapita <kartik.g@qapitacorp.com>
2022-08-09 17:43:45 +0530

    updated wallpaper script

--- Wall/wallpaper_schedule.sh
+++ Wall/wallpaper_schedule.sh
@@ -7,7 +7,7 @@ sudo apt install libglib2.0-dev-bin
 
 wget -qO - https://github.com/PRATAP-KUMAR/ubuntu-gdm-set-background/archive/main.tar.gz | tar zx --strip-components=1 ubuntu-gdm-set-background-main/ubuntu-gdm-set-background
 
-sudo wget https://raw.githubusercontent.com/kartik-qapita/kartik-qapita/main/wall.sh --output-document=/usr/share/qapita/wall.sh
+sudo wget https://raw.githubusercontent.com/kartik-qapita/kartik-qapita/main/Wall/wall.sh --output-document=/usr/share/qapita/wall.sh
 
 sudo chmod +x /usr/share/qapita/wall.sh
 
